d25d095395fac293c4757ee4a7c285b939a84990
FernandoMos92 <fernandomosp@gmail.com>
2022-08-19 11:24:15 -0300

    :recycle: refactor: rename UserContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { AppProps } from 'next/app'
 import GlobalStyle from '../styles/globals'
 
-import { UserContextProvider } from '../Context/Context'
+import { UserContextProvider } from '../Context/UserContext'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
