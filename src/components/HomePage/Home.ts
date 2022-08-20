30d8916da86175bfd4deea17307fa37609273444
FernandoMos92 <fernandomosp@gmail.com>
2022-08-20 07:46:12 -0300

    :recycle: refactor: added the calendar to the left space

--- src/components/HomePage/Home.ts
+++ src/components/HomePage/Home.ts
@@ -8,59 +8,7 @@ const Home = styled.main`
   .container__home-main {
     width: 100%;
   }
-  /* .calendar__container {
-    border: 1px solid #E8E8E8;
-    margin-bottom: 32px;
-    padding: 24px;
-    height: 375px;
-    width: 420px;
-    .calendar__container-month {
-      display: flex;
-      justify-content: space-between;
-      width: 365px;
-    }
-
-    .calendar__container-nameMonth {
-      font-size: 24px;
-      margin-bottom: 24px;
-    }
-
-     .rc_header_nav.rc_header_nav-prev > span {
-      visibility: hidden;
-    } 
-    
-    .rc_header_nav.rc_header_nav-next > span {
-      visibility: hidden;
-    }
-
-    .rc_header_nav.rc_header_nav-prev {
-      position: relative;
-    }
-
-    .rc_header_nav.rc_header_nav-prev::after {
-    content: " ";
-    background-image: url('/images/arrow-left.svg');
-    background-size: contain;
-    background-repeat: no-repeat;
-    position: absolute;
-    height: 10px;
-    width:10px;
-    top: .8rem;
-    left: .8rem;
-    }
-    .rc_header_nav.rc_header_nav-next::before {
-      content: " ";
-      background-image: url('/images/arrow-right.svg');
-      background-size: contain;
-      background-repeat: no-repeat;
-      position: absolute;
-      height: 10px;
-      width:10px;
-      top: 1rem;
-      right: 1rem;
-    }
-  }
-
+  /*
   .calendar__daily {
     border-top: 1px solid #E8E8E8;
     border-left: 1px solid #E8E8E8;
--- src/components/MainComponent/index.jsx
+++ src/components/MainComponent/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import MyCalendar from '../Calendar'
 
 const Main = styled.main`
   border: 2px solid black;
@@ -8,7 +9,6 @@ const Main = styled.main`
 
   .mainComponent__left,
   .mainComponent__right {
-    border: 2px solid black;
   }
 
   .mainComponent__left {
@@ -24,12 +24,66 @@ const Main = styled.main`
   .mainComponent__separatorLine {
     margin: 0 40px;
   }
+
+  .calendar__container {
+    border: 1px solid #e8e8e8;
+    margin-bottom: 32px;
+    height: 355px;
+    width: 100%;
+    .calendar__container-month {
+      display: flex;
+      justify-content: space-between;
+      width: 365px;
+    }
+
+    .calendar__container-nameMonth {
+      font-size: 13px;
+      margin-bottom: 24px;
+    }
+
+    .rc_header_nav.rc_header_nav-prev > span {
+      visibility: hidden;
+    }
+
+    .rc_header_nav.rc_header_nav-next > span {
+      visibility: hidden;
+    }
+
+    .rc_header_nav.rc_header_nav-prev {
+      position: relative;
+    }
+
+    .rc_header_nav.rc_header_nav-prev::after {
+      content: ' ';
+      background-image: url('/images/arrow-left.svg');
+      background-size: contain;
+      background-repeat: no-repeat;
+      position: absolute;
+      height: 10px;
+      width: 10px;
+      top: 0.8rem;
+      left: 0.8rem;
+    }
+    .rc_header_nav.rc_header_nav-next::before {
+      content: ' ';
+      background-image: url('/images/arrow-right.svg');
+      background-size: contain;
+      background-repeat: no-repeat;
+      position: absolute;
+      height: 10px;
+      width: 10px;
+      top: 1rem;
+      right: 1rem;
+    }
+  }
 `
 
 function MainComponent () {
   return (
     <Main>
-      <section className='mainComponent__left'>Esquerda</section>
+      <section className='mainComponent__left'>
+        <MyCalendar />
+      </section>
       <hr className='mainComponent__separatorLine' />
       <section className='mainComponent__right'>Direita</section>
     </Main>
